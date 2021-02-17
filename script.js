801bb99ea93c7518c6079e7b6c5e5354d00c9484
dakota <kotalilyy@gmail.com>
2021-02-17 06:32:33 -0600

    add jscript

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-alert("Let's generate your password!");
+const empty = "";
+const uCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lCase = "abcdefghijklmnopqrstuvwxyz";
+const number = "0123456789";
+const empty = "*&^%$#@!()=+-_";
+
+const pLength = document.getElementById("p-length");
+const upperCase = document.getElementById("p-uppercase");
+const lowerCase = document.getElementById("p-lowercase");
+const pNumber = document.getElementById("p-number");
+const pSymbol = document.getElementById("p-symbol");
+const submit = document.getElementById("submit");
+const password = document.getElementById("password")
+
