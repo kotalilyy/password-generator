f863343d7ccce3c6c9a14234c3bbaa426647e8d8
dakota <kotalilyy@gmail.com>
2021-02-17 06:41:16 -0600

    add for loop and math.random

--- script.js
+++ script.js
@@ -10,5 +10,23 @@ const lowerCase = document.getElementById("p-lowercase");
 const pNumber = document.getElementById("p-number");
 const pSymbol = document.getElementById("p-symbol");
 const submit = document.getElementById("submit");
-const password = document.getElementById("password")
+const password = document.getElementById("password");
+
+submit.addEventListener("click", () => {
+    let initialPassword = empty;
+    (upperCase.checked) ? initialPassword += uCase : "";
+    (lowere.checked) ? initialPassword += lCase : "";
+    (pNumber.checked) ? initialPassword += number : "";
+    (pSymbol.checked) ? initialPassword += symbol : "";
+
+    password.value = generatePassword(pLength.value, initialPassword)
+});
+
+function generatePassword(l, initialPassword) {
+    let pass = "";
+    for (let i = 0; i < l; i++){
+        pass += initialPassword.charAt(Math.floor(Math.random() * initialPassword.length));
+    }
+    return pass;
+}
 
