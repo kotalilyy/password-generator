241655cf446f32717e41002742dd9621e4426017
Dakota Nelson <kotalilyy@gmail.com>
2021-08-20 23:18:00 -0500

    format

--- README.md
+++ README.md
@@ -6,7 +6,6 @@ Use this Password Generator to generate a random password for you!
 
 https://kotalilyy.github.io/JavaScript-password-generator/
 
-
 ## Screenshot 
 
 <img width="576" alt="Screen Shot 2021-02-17 at 7 12 22 AM" src="https://user-images.githubusercontent.com/77229281/108209210-a6a11000-70ef-11eb-94b4-975241fee683.png">
--- js/script.js
+++ js/script.js
@@ -13,20 +13,21 @@ const submit = document.getElementById("submit");
 const password = document.getElementById("password");
 
 submit.addEventListener("click", () => {
-    let initialPassword = empty;
-    (upperCase.checked) ? initialPassword += uCase : "";
-    (lowerCase.checked) ? initialPassword += lCase : "";
-    (pNumber.checked) ? initialPassword += number : "";
-    (pSymbol.checked) ? initialPassword += symbol : "";
+  let initialPassword = empty;
+  upperCase.checked ? (initialPassword += uCase) : "";
+  lowerCase.checked ? (initialPassword += lCase) : "";
+  pNumber.checked ? (initialPassword += number) : "";
+  pSymbol.checked ? (initialPassword += symbol) : "";
 
-    password.value = generatePassword(pLength.value, initialPassword)
+  password.value = generatePassword(pLength.value, initialPassword);
 });
 
 function generatePassword(l, initialPassword) {
-    let pass = "";
-    for (let i = 0; i < l; i++) {
-        pass += initialPassword.charAt(Math.floor(Math.random() * initialPassword.length));
-    }
-    return pass;
+  let pass = "";
+  for (let i = 0; i < l; i++) {
+    pass += initialPassword.charAt(
+      Math.floor(Math.random() * initialPassword.length)
+    );
+  }
+  return pass;
 }
-
